c9b052385bdbeeecd38f515dd918fce2db02e183
Rhys Miller <rjmills87@aol.com>
2023-02-05 09:14:22 +0000

    Added 64 x 64 grid size.

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-const gridLayoutSizes = [16 * 16, 24 * 24, 32 * 32];
+const gridLayoutSizes = [16 * 16, 24 * 24, 32 * 32, 64 * 64];
 const gridContainer = document.querySelector(".grid-container");
 const grid16 = document.createElement("div");
 const grid24 = document.createElement("div");
 const grid32 = document.createElement("div");
+const grid64 = document.createElement("div");
 const btn16 = document.querySelector(".sixteen-btn");
 const btn24 = document.querySelector(".twentyfour-btn");
 const btn32 = document.querySelector(".thirtytwo-btn");
+const btn64 = document.querySelector(".sixtyfour-btn");
 const colorPicker = document.getElementById("color-pick");
-const clearBtn = document.querySelector("#clear-btn");
+const clearBtn = document.querySelector(".clear-btn");
 let previousGrid;
 
 //Functions to create different size grids
@@ -82,31 +84,76 @@ function createGrid32() {
   }
 }
 
+function createGrid64() {
+  if (previousGrid) {
+    gridContainer.removeChild(previousGrid);
+  }
+
+  previousGrid = grid64;
+  grid64.classList.add("grid64");
+  gridContainer.appendChild(grid64);
+
+  for (let cell = 0; cell < gridLayoutSizes[3]; cell++) {
+    let gridCell = document.createElement("div");
+    gridCell.classList.add("gridCell");
+    grid64.appendChild(gridCell);
+    gridCell.addEventListener("mouseover", function () {
+      gridCell.style.backgroundColor = colorPicker.value;
+    });
+    clearBtn.addEventListener("click", function () {
+      gridCell.style.backgroundColor = "white";
+    });
+  }
+}
+
 //Selection Buttons
 
 btn16.addEventListener("click", () => {
   createGrid16();
   grid24.textContent = "";
   grid32.textContent = "";
+  grid64.textContent = "";
   btn16.disabled = true;
   btn24.disabled = false;
   btn32.disabled = false;
+  btn64.disabled = false;
 });
 
 btn24.addEventListener("click", () => {
   createGrid24();
   grid16.textContent = "";
   grid32.textContent = "";
+  grid64.textContent = "";
   btn16.disabled = false;
   btn24.disabled = true;
   btn32.disabled = false;
+  btn64.disabled = false;
 });
 
 btn32.addEventListener("click", () => {
   createGrid32();
   grid16.textContent = "";
   grid24.textContent = "";
+  grid64.textContent = "";
   btn16.disabled = false;
   btn24.disabled = false;
   btn32.disabled = true;
+  btn64.disabled = false;
 });
+
+btn64.addEventListener("click", () => {
+  createGrid64();
+  grid16.textContent = "";
+  grid24.textContent = "";
+  grid32.textContent = "";
+  btn16.disabled = false;
+  btn24.disabled = false;
+  btn32.disabled = false;
+  btn64.disabled = true;
+});
+
+// eraserBtn.addEventListener("click", () => {
+//   gridCell.addEventListener("mousedown", function () {
+//     gridCell.style.backgroundColor = "white";
+//   });
+// })
